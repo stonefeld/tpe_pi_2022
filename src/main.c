05fa4522ea2ef379ade23f1bb9435ec050ca8936
PakiQuian <f.quianblanco@gmail.com>
2022-07-06 00:17:11 -0300

    Comenzando a analizar archivos

--- src/main.c
+++ src/main.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_args(int argc, char **argv);
-int file_exists(char *name);
+#define MAX_LENGTH 1024
 
 int
 main(int argc, char **argv)
 {	
-	check_args(argc, argv);
-	return 0;
-}
-
-//Realiza programacion defensiva chequeando que existan los archivos y que la cantidad de args recividos sea la correcta
-void
-check_args(int argc, char **argv)
-{
 	if (argc != 3) {
 		printf("La cantidad de argumentos proporcionados es distinta de dos\n");
-		exit(1);
+		return 1;
 	}
 	
-	if( file_exists(argv[1]) == 0 ){
-		printf("El archivo %s no existe\n", argv[1]);
-		exit(1);
-	}
-	
-	if(file_exists(argv[2]) == 0){
-		printf("El archivo %s no existe\n", argv[2]);
-		exit(1);
-	}
-
-}
-
-//Devuelve 0 si el archivo no existe
-int
-file_exists(char *name)
-{
 	FILE *f;
-	if ((f = fopen(name, "r")) == NULL) {
-		return 0;
+	char datos[];
+	Sensors s = NULL;
+	int c = 0;
+	
+	if ( (f = fopen(argv[1], "r")) == NULL ) {
+		printf("El archivo %s no existe\n", argv[1]);
+		return 1;
 	}
+	
+	fgets(datos, MAX_LENGTH, f);
+
+	while( fgets(datos, MAX_LENGTH, f) != NULL && c == 0){
+		c = sensors_add(s, datos);
+	}
+	
 	fclose(f);
-	return 1;
+	
+	if ((f = fopen(argv[2], "r")) == NULL) {
+		printf("El archivo %s no existe\n", argv[2]);
+		return 1;
+	}
+	
+	fgets(datos, MAX_LENGTH, f);
+
+	while( fgets(datos, MAX_LENGTH, f) != NULL && c == 0){
+		c = query_add(s, datos);
+	}
+	
+	fclose(f);
+
+	return c;
 }
+
