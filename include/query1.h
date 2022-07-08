6acc92476adebce86ba642abae477c02c6539b08
PakiQuian <f.quianblanco@gmail.com>
2022-07-08 12:00:37 -0300

    Documentacion de query 1, 2, 3 y sensors

--- include/query1.h
+++ include/query1.h
@@ -6,9 +6,29 @@
 
 typedef List Query1;
 
+/*
+*	Crea una lista encadenada de los datos de la query 1 vacia
+*/
 Query1 query1_new(void);
+
+/*
+*	Llama a una funcion la cual agrega ordenadamente un elemento a la lista de la query 1, pasando como argumentos funcion de comparacion y funcion de igualdad 
+*	A este elemento se le asigna el nombre, el id y el count pasados como argumentos
+*	En caso de error retorna un ErrorCode y en el caso de no encontrar error retorna NOE
+*/
 ErrorCodes query1_add(Query1 self, unsigned int id, char *name, unsigned int count);
+
+/*
+*	Antes de realizar la matriz, esta funcion llama al funcion que ordena los elementos de la query 1
+*	Se ordena utilizando count (de mayor a menor) y en caso de igualdad se desempata por nombre
+*	Luego llama a la funcion que genera la matriz
+*	En caso de error retorna un ErrorCode y en el caso de no encontrar error retorna NOE
+*/
 ErrorCodes query1_tomatrix(Query1 self, Matrix *mat, unsigned int *rows, unsigned int *cols);
+
+/*
+*	Libera los recursos usados por la query 1
+*/
 void query1_free(Query1 self);
 
 void query1_print(Query1 self);
--- include/query2.h
+++ include/query2.h
@@ -6,9 +6,27 @@
 
 typedef List Query2;
 
+/*
+*	Crea una lista encadenada de los datos de la query 2 vacia
+*/
 Query2 query2_new(void);
+
+/*
+*	Llama a una funcion la cual agrega ordenadamente un elemento a la lista de la query 2, pasando como argumentos funcion de comparacion y funcion de igualdad 
+*	A este elemento se le asigna el año y el count pasados como argumentos
+*	En caso de error retorna un ErrorCode y en el caso de no encontrar error retorna NOE
+*/
 ErrorCodes query2_add(Query2 self, unsigned int year, unsigned int count);
+
+/*
+*	Llama a la funcion que genera la matriz
+*	En caso de error retorna un ErrorCode y en el caso de no encontrar error retorna NOE
+*/
 ErrorCodes query2_tomatrix(Query2 self, Matrix *mat, unsigned int *rows, unsigned int *cols);
+
+/*
+*	Libera los recursos usados por la query 2
+*/
 void query2_free(Query2 self);
 
 void query2_print(Query2 self);
